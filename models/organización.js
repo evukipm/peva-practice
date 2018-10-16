c5fbc357078b38768c3ab026a975c811df6bc2fa
Alexandra Pedroche <alexandrapedroche@gmail.com>
2018-10-16 17:56:34 +0200

    hacer modelos

---
+++ models/organización.js
@@ -0,0 +1,16 @@
+const mongoose = require ("mongoose");
+const Schema = mongoose.Schema;
+
+const OrganizacionSchema = new Schema ( {
+  name: String,
+  level: {type: String, enum: ["Cutre", "Ni pa ti ni pa mi", "Tocha"],
+  origin: String,
+  catchPhrase: String,
+  description: String,
+  normas: Array,
+});
+
+const Organizacion = mongoose.model("Organizacion", organizacionSchema);
+
+module.exports = Organizacion;
+
